88a997c0681170a302444b462004bdeaeaabe03f
Alexsandro <Alex_ctba@icloud.com>
2020-11-12 15:23:09 -0300

    Iniciando Projeto Consumindo API

---
+++ main.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+class api{
+    constructor(){
+        this.dados = [];
+        this.form = document.querySelector('#form');
+        this.input = document.querySelector('input[name=user]')
+        
+        console.log(input)
+    }
+}
+
+
+new api()
